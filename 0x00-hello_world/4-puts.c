72188032ad2dd8f864749a86e9026d15cedfa3c2
Ct410 <picturesbyct@gmail.com>
2022-06-30 17:12:57 -0700

    hello 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,13 @@
 #include <string.h>
 
 /**
- *  * main - Entry point
- *   * Description: prints "\"Programming is like building a multilingual puzzle"*
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"*
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
